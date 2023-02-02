27568e2a7208b0ce0dc4186b510d9034c2515409
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-02-02 12:10:17 +0200

    Assets and simple function when screen is touched

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -12,10 +12,25 @@ class GameScene: SKScene {
   
     
     override func didMove(to view: SKView) {
+        // Set background picture to be shown at all time
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
-
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
     }
 
-
+    // Show touch location at the screen
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else{return}
+        let location = touch.location(in: self)
+        
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+        box.position = location
+        addChild(box)
+    }
 }
